20f4f4e03fc55b6c75f3802c5d7532ea9fe784d6
Khoa Tran <jacktran1799@gmaail.com>
2020-09-02 11:21:42 -0500

    completed prime number stretch

---
+++ src/16-PrimeNumberStretch.py
@@ -0,0 +1,15 @@
+def isPrimeNum():
+    while True:
+        num=int(input('Give any number greater than 1 to evaluate: '))
+        if num == 1:
+            continue
+        else:
+            for i in range(2,num):
+                if num % i == 0:
+                    print(f'{num} is not a prime number')
+                    break
+            else:
+                print(f'{num} is a prime number')
+            break
+            
+isPrimeNum()
